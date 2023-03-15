99292b303bbc2b614f544c04be31ba7bdf1d2b27
emmaparis <elp.dance@gmail.com>
2023-03-14 21:37:32 -0400

    schema commits

--- Unsolved/db/schema.sql
+++ Unsolved/db/schema.sql
@@ -7,7 +7,7 @@ USE employee_tracker_db;
 -- department table 
 CREATE TABLE department (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(30) 
+  name VARCHAR(30) NOT NULL
 );
 
 -- roles table 
@@ -16,6 +16,9 @@ CREATE TABLE roles (
   title VARCHAR(30),
   salary DECIMAL, 
   department_id INT
+  FOREIGN KEY (department_id)
+  REFERENCES department_id
+  ON DELETE SET NULL
 );
 
 -- employees table 
@@ -23,6 +26,12 @@ CREATE TABLE employees (
   id INT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30), 
- role_id INT,
+ role_id INT
+ FOREIGN KEY (role_id)
+ REFERENCES roles(id)
+ ON DELETE SET NULL
  manager_id INT NULL
+ FOREIGN KEY (manager_id)
+ REFERENCES employees(id)
+ ON DELETE SET NULL
 );
